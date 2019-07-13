fb53f728655ccf131f1e1d5aa376cbc5b3a6940b
胡朋（小莫） <xiaomo@xiaomo.info>
2019-07-13 21:41:46 +0900

    tabs

--- app/src/main/java/com/example/orc_demo/MainActivity.java
+++ app/src/main/java/com/example/orc_demo/MainActivity.java
@@ -22,14 +22,5 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
--- app/src/main/java/com/example/orc_demo/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/orc_demo/ui/main/SectionsPagerAdapter.java
@@ -1,11 +1,13 @@
 package com.example.orc_demo.ui.main;
 
 import android.content.Context;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+
 import com.example.orc_demo.R;
 
 /**
@@ -15,7 +17,11 @@ import com.example.orc_demo.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[] {R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{
+            R.string.tab_text_1,
+            R.string.tab_text_2,
+            R.string.tab_text_3
+    };
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -25,8 +31,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
         return PlaceholderFragment.newInstance(position + 1);
     }
 
@@ -38,7 +42,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        return 3;
     }
 }
